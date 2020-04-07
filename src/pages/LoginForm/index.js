3e3bb0b9794b35dfe57cbbb577c312a88591965d
Beatriz Miranda <biia.mfs@gmail.com>
2020-04-06 22:52:47 -0300

    Login change icon eye

--- src/pages/LoginForm/index.js
+++ src/pages/LoginForm/index.js
@@ -1,40 +1,50 @@
-import React from 'react';
-import { View, TextInput, Text, TouchableOpacity, Image } from 'react-native';
-import { Formik } from 'formik';
-import { Feather } from '@expo/vector-icons';
+import React, { useState } from "react";
+import { View, TextInput, Text, TouchableOpacity, Image } from "react-native";
+import { Formik } from "formik";
+import { Feather } from "@expo/vector-icons";
 
-import styles from './styles';
+import styles from "./styles";
 
 import googleIcon from "../../assets/logar-google.png";
 import faceIcon from "../../assets/logar-facebook.png";
 
-
 export default function LoginForm() {
+    const [showPass, setShowPassword] = useState(false);
+    const [eyeIcon, setEyeIcon] = useState("eye-off");
+
+    const showPassword = () => {
+        console.log("click");
+        setShowPassword(!showPass);
+        showPass ? setEyeIcon("eye") : setEyeIcon("eye-off");
+    };
     return (
         <View style={styles.loginFormContainer}>
             <Formik
-                initialValues={{ email: '', password: '' }}
-                onSubmit={() => { }}
+                initialValues={{ email: "", password: "" }}
+                onSubmit={() => {}}
             >
-
-                {formikProps => (
+                {(formikProps) => (
                     <View style={styles.inputContainer}>
                         <TextInput
                             style={styles.emailInput}
-                            placeholder='E-mail'
+                            placeholder="E-mail"
                             value={formikProps.values.email}
-                            onChangeText={formikProps.handleChange('email')}
-                            onBlur={formikProps.handleBlur('email')}
+                            onChangeText={formikProps.handleChange("email")}
+                            onBlur={formikProps.handleBlur("email")}
                         />
-                        <TouchableOpacity onPress={() => console.log('eye clicked')}>
-                            <Feather name='eye-off' size={18} style={styles.eye} />
+                        <TouchableOpacity onPress={() => showPassword()}>
+                            <Feather
+                                name={eyeIcon}
+                                size={18}
+                                style={styles.eye}
+                            />
                         </TouchableOpacity>
                         <TextInput
                             style={styles.passwordInput}
-                            placeholder='Senha'
+                            placeholder="Senha"
                             value={formikProps.values.password}
-                            onChangeText={formikProps.handleChange('password')}
-                            onBlur={formikProps.handleBlur('password')}
+                            onChangeText={formikProps.handleChange("password")}
+                            onBlur={formikProps.handleBlur("password")}
                             secureTextEntry={true}
                         />
 
@@ -43,17 +53,21 @@ export default function LoginForm() {
                                 <Text style={styles.btnText}>Entre</Text>
                             </TouchableOpacity>
                             <TouchableOpacity>
-                                <Image source={googleIcon} style={styles.googleIcon} />
+                                <Image
+                                    source={googleIcon}
+                                    style={styles.googleIcon}
+                                />
                             </TouchableOpacity>
                             <TouchableOpacity>
-                                <Image source={faceIcon} style={styles.faceIcon} />
+                                <Image
+                                    source={faceIcon}
+                                    style={styles.faceIcon}
+                                />
                             </TouchableOpacity>
                         </View>
-
                     </View>
                 )}
-
             </Formik>
         </View>
-    )
+    );
 }
