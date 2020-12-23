5a77bbc2cd3f4db5e2bb202c7a91e6b50faa14df
Beatriz Miranda <beatriz.miranda@acct.global>
2020-12-23 15:31:31 -0300

    removing unused code

--- src/Components/HomeLogged/index.js
+++ src/Components/HomeLogged/index.js
@@ -1,5 +1,12 @@
-import React, { useState } from "react";
-import { View, Text, TouchableOpacity, Image, FlatList, KeyboardAvoidingView, Platform } from "react-native";
+import React from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  FlatList,
+  KeyboardAvoidingView,
+} from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import Swipeable from "react-native-swipeable";
 import { Feather } from "@expo/vector-icons";
@@ -12,87 +19,76 @@ import profilePic from "../../assets/defaultUserImage.png";
 import sendMail from "../../assets/Logar-Email.png";
 
 const btns = [
-    <TouchableOpacity onPress={() => { }} style={styles.touch}>
-        <Feather
-            name="trash-2"
-            style={styles.deleteText}
-            size={27}
-            color="#fff"
-        />
-    </TouchableOpacity>,
+  <TouchableOpacity onPress={() => {}} style={styles.touch}>
+    <Feather name="trash-2" style={styles.deleteText} size={27} color="#fff" />
+  </TouchableOpacity>,
 ];
 
 function HomeLogged(props) {
-    const { favorProfs } = props;
+  const { favorProfs } = props;
 
-    const msg = "Nenhum professor salvo.";
-    const msg2 = "Comece agora!";
+  const msg = "Nenhum professor salvo.";
+  const msg2 = "Comece agora!";
 
-    const navigation = useNavigation();
+  const navigation = useNavigation();
 
-    const deleteProf = (id) => {
-        props.deleteProf(id);
-    };
+  const deleteProf = (id) => {
+    props.deleteProf(id);
+  };
 
-    const toFavorites = () => {
-        navigation.navigate("Favorites");
-    };
+  const toFavorites = () => {
+    navigation.navigate("Favorites");
+  };
 
-    return favorProfs.length ? (
-        <View style={styles.profListContainer}>
-            <KeyboardAvoidingView behavior='padding' style={{ flex: 1 }}>
-                <FlatList
-                    style={styles.favList}
-                    data={favorProfs}
-                    showsVerticalScrollIndicator={false}
-                    keyExtractor={(favorProfs) => String(favorProfs.id)}
-                    renderItem={({ item: prof }) => (
-                        <Swipeable
-                            leftButtons={btns}
-                            onLeftActionRelease={() => deleteProf(prof.id)}
-                        >
-                            <TouchableOpacity onPress={toFavorites}>
-                                <View style={styles.listContainer}>
-                                    <Image
-                                        source={profilePic}
-                                        style={styles.profilePic}
-                                    />
-                                    <Text style={styles.profName}>{prof.name}</Text>
-                                    <TouchableOpacity
-                                        onPress={() => console.log("mail composer")}
-                                    >
-                                        <Image
-                                            source={sendMail}
-                                            style={styles.sendMail}
-                                        />
-                                    </TouchableOpacity>
-                                </View>
-                            </TouchableOpacity>
-                        </Swipeable>
-                    )}
-                />
-            </KeyboardAvoidingView>
-        </View>
-    ) : (
-            <View style={styles.profListContainer}>
-                <Empty msg={msg} msg2={msg2} />
-            </View>
-        );
+  return favorProfs.length ? (
+    <View style={styles.profListContainer}>
+      <KeyboardAvoidingView behavior="padding" style={{ flex: 1 }}>
+        <FlatList
+          style={styles.favList}
+          data={favorProfs}
+          showsVerticalScrollIndicator={false}
+          keyExtractor={(favorProfs) => String(favorProfs.id)}
+          renderItem={({ item: prof }) => (
+            <Swipeable
+              leftButtons={btns}
+              onLeftActionRelease={() => deleteProf(prof.id)}
+            >
+              <TouchableOpacity onPress={toFavorites}>
+                <View style={styles.listContainer}>
+                  <Image source={profilePic} style={styles.profilePic} />
+                  <Text style={styles.profName}>{prof.name}</Text>
+                  <TouchableOpacity
+                    onPress={() => console.log("mail composer")}
+                  >
+                    <Image source={sendMail} style={styles.sendMail} />
+                  </TouchableOpacity>
+                </View>
+              </TouchableOpacity>
+            </Swipeable>
+          )}
+        />
+      </KeyboardAvoidingView>
+    </View>
+  ) : (
+    <View style={styles.profListContainer}>
+      <Empty msg={msg} msg2={msg2} />
+    </View>
+  );
 }
 
 const mapStateToProps = (state) => {
-    return {
-        user: state.user,
-        favorProfs: state.user.favorProfs,
-    };
+  return {
+    user: state.user,
+    favorProfs: state.user.favorProfs,
+  };
 };
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-        deleteProf: (id) => {
-            dispatch({ type: "DELETE_PROF", id: id });
-        },
-    };
+  return {
+    deleteProf: (id) => {
+      dispatch({ type: "DELETE_PROF", id: id });
+    },
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(HomeLogged);
